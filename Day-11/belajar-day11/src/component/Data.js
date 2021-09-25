d6175ea7d95f30f43a7ad95d8a49b6345d2101c4
Muhammad Ikram Kaer Sinapoy <59946070+ikramsinapoy@users.noreply.github.com>
2021-09-26 05:04:41 +0700

    Delete Day-11/belajar-day11 directory

--- Day-11/belajar-day11/src/component/Data.js
+++
@@ -1,8 +0,0 @@
-const Data = [
-  { id: 1, title: "Membuat Komponen", completed: true },
-  { id: 2, title: "Unit Testing", completed: false },
-  { id: 3, title: "Setup Development Environment", completed: true },
-  { id: 4, title: "Deploy ke Server", completed: false },
-];
-
-export default Data;
